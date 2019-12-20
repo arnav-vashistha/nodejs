9a41eed9fa852df2ccb48c8f996d2f3d2827f88f
Arnav Vashistha <M1030112@mindtree.com>
2019-12-20 17:52:11 +0530

    Mongoose Part 1

---
+++ node-mongoose/index.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/confusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+
+    console.log('Connected correctly to server');
+
+    var newDish = Dishes({
+        name: 'Uthappizza',
+        description: 'test'
+    });
+
+    newDish.save()
+        .then((dish) => {
+            console.log(dish);
+
+            return Dishes.find({});
+        })
+        .then((dishes) => {
+            console.log(dishes);
+
+            return Dishes.remove({});
+        })
+        .then(() => {
+            console.log("closing connection");
+            return mongoose.connection.close();
+
+        }).then(()=>{
+            console.log("connection closed");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+});
